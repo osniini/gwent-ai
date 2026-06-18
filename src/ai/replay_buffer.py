2eea0c0ed7f84580d5fe4fbd751c67207afe48d7
Oskari Niini <oskari.niini@gmail.com>
2026-06-18 21:53:31 +0300

    Replay buffer for training

---
+++ src/ai/replay_buffer.py
@@ -0,0 +1,26 @@
+import random
+import collections
+import numpy as np
+
+class ReplayBuffer:
+    def __init__(self, capacity: int):
+        self.buffer = collections.deque(maxlen=capacity)
+
+    def push(self, state, action, reward, next_state, done):
+        self.buffer.append((state, action, reward, next_state, done))
+
+    def sample(self, batch_size: int):
+        batch = random.sample(self.buffer, batch_size)
+
+        states, actions, rewards, next_states, dones = zip(*batch)
+
+        return (
+            np.array(states, dtype=np.float32),
+            np.array(actions, dtype=np.int64),
+            np.array(rewards, dtype=np.float32),
+            np.array(next_states, dtype=np.float32),
+            np.array(dones, dtype=np.bool_),
+        )
+
+    def __len__(self):
+        return len(self.buffer)
